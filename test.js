7d278f82c538da656cf5f0078e563fcab74581c7
dracupid <dracupid@gmail.com>
2014-11-25 02:00:28 +0800

    correct the use of asny

--- test.js
+++ test.js
@@ -20,37 +20,43 @@ describe('Promise', function() {
         })
     })
     describe('#then', function() {
-        it('should print the value resolved', function() {
+        it('should print the value resolved', function(done) {
             var promise = new Promise(function(resolve, reject) {
                 resolve(13578)
             });
             promise.then(function(rs) {
                 assert.equal(rs, 13578)
+                done()
             }, function(err) {
                 assert.equal(typeof err, typeof undefined)
+                done()
             });
         })
-        it('should print the value rejected', function() {
+        it('should print the value rejected', function(done) {
             var promise = new Promise(function(resolve, reject) {
                 reject("abc")
             })
             promise.then(function(rs) {
                 assert.equal(typeof rs, typeof undefined);
+                done()
             }, function(err) {
                 assert.equal(err, "abc");
+                done()
             });
         });
-        it("should reject the Error thrown as a Promise", function() {
+        it("should reject the Error thrown as a Promise", function(done) {
             var promise = new Promise(function(resolve, reject) {
                     throw TypeError('hehe')
                 })
             promise.then(function(rs) {
                 assert.equal(typeof rs, typeof undefined);
+                done()
             }, function(err) {
                 assert.equal(typeof err, 'object');
+                done()
             })
         })
-        it('should resolve the return value for handler', function() {
+        it('should resolve the return value for handler', function(done) {
             var promise = new Promise(function(resolve, reject) {
                 resolve(13578)
             });
@@ -59,42 +65,48 @@ describe('Promise', function() {
                 return 12345
             }, function(err) {
                 assert.equal(typeof err, typeof undefined)
+                done()
             }).then(function(rs){
                 assert(rs, 12345)
-                Promise.reject('err')
+                return Promise.reject('err')
             }, function(err){
                 assert.equal(typeof err, typeof undefined)
+                done()
             })['catch'](function(err){
                 assert.equal(err, 'err')
                 return 13579
             }).then(function(rs){
                 assert.equal(rs, 13579)
+                done()
             }, function(err){
                 assert.equal(typeof err, typeof undefined)
+                done()
             })
         })
     });
     describe('#Promise.resolve and Promise.reject', function() {
-        it("should reolve the value as a Promise", function() {
+        it("should reolve the value as a Promise", function(done) {
             var obj = {
                     ad: 213
                 },
                 promise = new Promise(function(resolve, reject) {
                     resolve(obj)
                 })
-
             promise.then(function(rs) {
                 assert.equal(rs, obj);
-                Promise.resolve(rs)
+                return Promise.resolve(rs)
             }, function(err) {
                 assert.equal(typeof err, typeof undefined)
+                done()
             }).then(function(rs) {
                 assert.equal(rs, obj);
+                done()
             }, function(err) {
                 assert.equal(typeof err, typeof undefined);
+                done()
             })
         })
-        it("should reject the value as a Promise", function() {
+        it("should reject the value as a Promise", function(done) {
             var arr = [1, 3, 4, 5, 7],
                 promise = new Promise(function(resolve, reject) {
                     resolve(arr);
@@ -102,83 +114,94 @@ describe('Promise', function() {
 
             promise.then(function(rs) {
                 assert.equal(rs, arr);
-                Promise.reject(rs);
+                return Promise.reject(rs);
             }, function(err) {
                 assert.equal(typeof err, typeof undefined);
+                done()
             }).then(function(rs) {
                 assert.equal(typeof rs, typeof undefined);
+                done()
             }, function(err) {
-                assert.equal(rs, err)
+                assert.equal(arr, err)
+                done()
             })
         })
     });
     describe("#catch", function(){
-        it("should catch the rejected reason", function(){
+        it("should catch the rejected reason", function(done){
             var promise = new Promise(function(resolve, reject) {
                 reject("abc")
             })
             promise.then(function(rs) {
                 assert.equal(typeof rs, typeof undefined);
+                done()
             })['catch'](function(err){
                 assert.equal(err, "abc")
+                done()
             })
         })
-        it("should catch the thrown error", function(){
+        it("should catch the thrown error", function(done){
             var promise = new Promise(function(resolve, reject) {
                 throw TypeError("abc")
             })
             promise.then(function(rs) {
+                assert.equal(typeof rs, typeof undefined);
+                done()
+            })['catch'](function(err){
+                assert.equal(typeof err, 'object')
+                done()
+            })
+        })
+        it("should catch after a long way passed", function(done){
+            var promise = new Promise(function(resolve, reject) {
+                throw TypeError("abc")
+            })
+            promise.then(function(rs) {
+                assert.equal(typeof rs, typeof undefined);
+            }).then(function(rs) {
+                assert.equal(typeof rs, typeof undefined);
+            }).then(function(rs) {
+                assert.equal(typeof rs, typeof undefined);
+            }).then(function(rs) {
+                assert.equal(typeof rs, typeof undefined);
+            }).then(function(rs) {
+                assert.equal(typeof rs, typeof undefined);
+            }).then(function(rs) {
+                assert.equal(typeof rs, typeof undefined);
+            }).then(function(rs) {
+                assert.equal(typeof rs, typeof undefined);
+            }).then(function(rs) {
+                assert.equal(typeof rs, typeof undefined);
+            }).then(function(rs) {
+                assert.equal(typeof rs, typeof undefined);
+            }).then(function(rs) {
+                assert.equal(typeof rs, typeof undefined);
+            }).then(function(rs) {
                 assert.equal(typeof rs, typeof undefined);
             })['catch'](function(err){
                 assert.equal(typeof err, 'object')
+                done()
             })
         })
-        it("should catch after a long way passed", function(){
+        it("should not catch after reject reason has been handled", function(done){
             var promise = new Promise(function(resolve, reject) {
                 throw TypeError("abc")
             })
             promise.then(function(rs) {
                 assert.equal(typeof rs, typeof undefined);
-            }).then(function(rs) {
-                assert.equal(typeof rs, typeof undefined);
-            }).then(function(rs) {
-                assert.equal(typeof rs, typeof undefined);
-            }).then(function(rs) {
-                assert.equal(typeof rs, typeof undefined);
-            }).then(function(rs) {
-                assert.equal(typeof rs, typeof undefined);
-            }).then(function(rs) {
-                assert.equal(typeof rs, typeof undefined);
-            }).then(function(rs) {
-                assert.equal(typeof rs, typeof undefined);
-            }).then(function(rs) {
-                assert.equal(typeof rs, typeof undefined);
-            }).then(function(rs) {
-                assert.equal(typeof rs, typeof undefined);
-            }).then(function(rs) {
-                assert.equal(typeof rs, typeof undefined);
-            }).then(function(rs) {
-                assert.equal(typeof rs, typeof undefined);
-            })['catch'](function(err){
-                assert.equal(typeof err, 'object')
-            })
-        })
-        it("should not catch after reject reason has been handled", function(){
-            var promise = new Promise(function(resolve, reject) {
-                throw TypeError("abc")
-            })
-            promise.then(function(rs) {
-                assert.equal(typeof rs, typeof undefined);
+                done()
             }, function(err){
                 assert.equal(typeof err, 'object')
+                done()
             })['catch'](function(err){
                 assert.equal(typeof err, typeof undefined)
+                done()
             })
         })
         
     })
     describe('#Promise.all', function() {
-        it('should resolve an array after all promises are resolved', function() {
+        it('should resolve an array after all promises are resolved', function(done) {
             var arr = [1, 2, 3],
                 promise1 = new Promise(function(re, rj){
                     re(arr[0])
@@ -195,10 +218,11 @@ describe('Promise', function() {
                 assert.equal(rs[0], arr[0]);
                 assert.equal(rs[1], arr[1]);
                 assert.equal(rs[2], arr[2]);
+                done()
             })
         });
 
-        it('should reject the array if one of the promises is rejected', function() {
+        it('should reject the array if one of the promises is rejected', function(done) {
             var arr = [1, 2, 3],
                 promise1 = new Promise(function(re, rj){
                     re(arr[0])
@@ -212,8 +236,10 @@ describe('Promise', function() {
             Promise.all([promise1, promise2, promise3])
             .then(function(rs){
                 assert.equal(typeof rs, typeof undefined)
+                done()
             }, function(err){
                 assert.equal(err, arr[1])
+                done()
             })
         });
     })
@@ -222,26 +248,28 @@ describe('Promise', function() {
             var promise = new Promise(function(re, rj){
                     setTimeout(function(){
                         re('good')
-                        done()
                     }, 1500)
                 })
             promise.then(function(rs){
                 assert.equal(rs, 'good')
+                done()
             })['catch'](function(err){
                 assert.equal(typeof err, typeof undefined)
+                done()
             })
         });
         it('should be rejected after 1.5 seconds', function(done) {
             var promise = new Promise(function(re, rj){
                     setTimeout(function(){
                         rj('bad')
-                        done()
                     }, 1500)
                 })
             promise.then(function(rs){
                 assert.equal(typeof rs, typeof undefined)
+                done()
             })['catch'](function(err){
                 assert.equal(err, 'bad')
+                done()
             })
         });
 
